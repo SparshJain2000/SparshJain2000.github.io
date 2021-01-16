fa3f3945b1eacd5db505d233374c3f2a94e6ba55
SparshJain2000 <jainsparsh0801@gmail.com>
2021-01-16 18:17:19 +0530

    change transitions

--- assets/js/slidein.js
+++ assets/js/slidein.js
@@ -1,4 +1,4 @@
-$(function () {
+$(function() {
     // $(document).ready shorthand
     $(".monster").fadeIn("slow");
     $("#front").hide();
@@ -6,17 +6,18 @@ $(function () {
     $("#comp").hide();
 });
 
-$(document).ready(function () {
+$(document).ready(function() {
     /* Every time the window is scrolled ... */
-    $(window).scroll(function () {
+    $(window).scroll(function() {
         /* Check the location of each desired element */
-        $(".hideme").each(function (i) {
+        $(".hideme").each(function(i) {
             var bottom_of_object = $(this).position().top;
             // $(this).position().top + $(this).outerHeight();
             var bottom_of_window = $(window).scrollTop() + $(window).height();
 
             /* If the object is completely visible in the window, fade it it */
-            if (bottom_of_window > bottom_of_object) {
+            console.log(bottom_of_window, bottom_of_object);
+            if (bottom_of_window > bottom_of_object + 60) {
                 // $(this).animate({ opacity: "1", right: "0" }, 100);
                 $(this).addClass("is-visible");
             }
