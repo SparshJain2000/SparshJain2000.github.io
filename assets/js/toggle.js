9518806c382519caa8638b40142ffce73a3c025d
SparshJain2000 <jainsparsh0801@gmail.com>
2021-01-28 13:47:43 +0530

    change skill toggler

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -1,29 +1,14 @@
+let openFields = {
+    front: false,
+    back: false,
+    comp: false,
+};
 toggleField = (e, id) => {
-    // if (id === "front") {
-    //     $(`#${id}`).slideToggle("slow");
-    //     $(`#back`).slideUp("slow");
-    //     $(`#comp`).slideUp("slow");
-    // }
-    // if (id === "back") {
-    //     $(`#${id}`).slideToggle("slow");
-    //     $(`#front`).slideUp("slow");
-    //     $(`#comp`).slideUp("slow");
-    // }
-    // if (id === "comp") {
-    //     $(`#${id}`).slideToggle("slow");
-    //     $(`#back`).slideUp("slow");
-    //     $(`#front`).slideUp("slow");
-    // }
-    const classes = $(e.target).attr("class");
-
-    $(e.target).attr(
+    openFields[id] = !openFields[id];
+    $(`#${id}-toggle`).attr(
         "class",
-        `${
-            classes === "fa fa-lg fa-angle-down"
-                ? "fa fa-lg fa-angle-up"
-                : "fa fa-lg fa-angle-down"
-        }`,
+        `${openFields[id] ? "fa fa-lg fa-angle-up" : "fa fa-lg fa-angle-down"}`,
     );
-    // $(e.target).attr("class", "fa fa-lg fa-angle-up");
+
     $(`#${id}`).slideToggle("slow");
 };
