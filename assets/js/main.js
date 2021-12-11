3ede62aa6a1860cde8d5763c0305b31df5813866
Sparsh Jain <jainsparsh0801@gmail.com>
2021-12-11 16:25:25 +0530

    add theme toggle images and color changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -137,12 +137,15 @@ const toDark = (event) => {
     document.querySelector("body").classList.remove("light");
     document.querySelector("body").classList.add("dark");
     event.value = "Dark";
-    event.classList.remove("btn-warning");
+    event.classList.remove("btn-light");
     event.classList.add("btn-secondary");
 
     document.head.querySelector('meta[name="theme-color"]').content = "#2a2e35";
-    document.querySelector("#theme").classList.remove("fa-sun");
-    document.querySelector("#theme").classList.add("fa-moon");
+    // document.querySelector("#theme").classList.remove("fa-sun");
+    document.querySelector("#theme").src = "/images/moon.png";
+    console.log(document.querySelector("#theme"));
+
+    // document.querySelector("#theme").classList.add("fa-moon");
     document.documentElement.setAttribute("data-theme", "dark");
     localStorage.setItem("theme", "dark");
 };
@@ -151,9 +154,12 @@ const toLight = (event) => {
     document.querySelector("body").classList.remove("dark");
     event.value = "Light";
     document.head.querySelector('meta[name="theme-color"]').content = "#ffffff";
-    document.querySelector("#theme").classList.remove("fa-moon");
-    document.querySelector("#theme").classList.add("fa-sun");
-    event.classList.add("btn-warning");
+    // document.querySelector("#theme").classList.remove("fa-moon");
+    // document.querySelector("#theme").classList.add("fa-sun");
+    document.querySelector("#theme").src = "/images/sun.png";
+    console.log(document.querySelector("#theme"));
+
+    event.classList.add("btn-light");
     event.classList.remove("btn-secondary");
     document.documentElement.setAttribute("data-theme", "light");
     localStorage.setItem("theme", "light");
