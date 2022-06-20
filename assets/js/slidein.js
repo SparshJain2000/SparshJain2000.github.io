9d89035547644c9b480cd912e1871707b9c0f661
Sparsh Jain <jainsparsh0801@gmail.com>
2022-06-20 17:45:18 +0530

    add js for animation

--- assets/js/slidein.js
+++ assets/js/slidein.js
@@ -31,3 +31,44 @@ $(document).ready(function () {
         });
     });
 });
+
+var textWrapper = document.querySelector(".ml10 .letters");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+    /\S/g,
+    "<span class='letter'>$&</span>",
+);
+
+anime
+    .timeline({ loop: true })
+    .add({
+        targets: ".ml2 .letter",
+        scale: [4, 1],
+        opacity: [0, 1],
+        translateZ: 0,
+        easing: "easeOutExpo",
+        duration: 950,
+        delay: (el, i) => 70 * i,
+    })
+    .add({
+        targets: ".ml2",
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000,
+    });
+anime
+    .timeline({ loop: true })
+    .add({
+        targets: ".ml10 .letter",
+        scale: [0, 1],
+        duration: 1500,
+        elasticity: 600,
+        delay: (el, i) => 45 * (i + 1),
+    })
+    .add({
+        targets: ".ml10",
+        opacity: 0.8,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 200,
+    });
