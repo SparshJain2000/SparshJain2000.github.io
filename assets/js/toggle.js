de8678941a5f073140c6e3dac5bee47c39635bce
Sparsh Jain <jainsparsh0801@gmail.com>
2023-05-14 23:24:23 +0530

    toggle skills

--- assets/js/toggle.js
+++ assets/js/toggle.js
@@ -5,10 +5,20 @@ let openFields = {
 };
 toggleField = (e, id) => {
     openFields[id] = !openFields[id];
+    if (openFields[id]) {
+        Object.keys(openFields).forEach((key) => key !== id && closeField(key));
+    }
     $(`#${id}-toggle`).attr(
         "class",
         `${openFields[id] ? "fa fa-lg fa-angle-up" : "fa fa-lg fa-angle-down"}`,
     );
     $(`#${id}Header`).toggleClass("selectedHeader");
     $(`#${id}`).slideToggle("slow");
+};
+closeField = (id) => {
+    console.log(id);
+    if (openFields[id]) $(`#${id}`).slideToggle("slow");
+    openFields[id] = false;
+    $(`#${id}-toggle`).attr("class", "fa fa-lg fa-angle-down");
+    $(`#${id}Header`).removeClass("selectedHeader");
 };
